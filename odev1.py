ede051fb85d64fe54e1cc3818a0a702f9c6d1b8d
ahsen11 <ahsnkyz@gmail.com>
2024-12-02 22:27:05 +0300

    odev1

---
+++ odev1.py
@@ -0,0 +1,49 @@
+# 1- "Toyota, Bmw, Renault, Mercedes" elemanlarına sahip markalar isimli bir liste oluşturunuz.
+markalar=["Toyota","Bmw","Renault","Mercedes"]
+print(markalar)
+
+# 2- Liste kaç elemanlıdır?
+print(len(markalar))
+
+# 3- Listenin ilk ve son elemanı nedir?
+print(markalar[0] + " " + markalar[-1])
+
+# 4- "Renault" markasını "Togg" ile güncelleyiniz.
+markalar[2]="Togg"
+
+# 5- "Togg" listenin bir elemanı mıdır?
+sonuc="Togg" in markalar
+print(sonuc)
+
+# 6- Listenin sonuna "Ford" ve "Citroen" markalarını ekleyiniz.
+markalar.append("Ford")
+markalar.append("Citroen")
+print(markalar)
+
+# 7- Listenin son elemanını siliniz.
+del(markalar[-1])
+print(markalar)
+
+# 8- Aşağıdaki verileri liste içerisinde saklayınız (Liste içinde liste mümkündür.).
+
+ # öğrenci1: Yiğit Bilgi 2010 [70,80,90]
+ # öğrenci2: Ada Bilgi   2011 [70,70,80]
+ # öğrenci3: Çınar Turan 2017 [60,60,90]
+
+ogrenci1=["Yiğit","Bilgi",2010,[70,80,90]]
+ogrenci2=["Ada","Bilgi",2011,[70,70,80]]
+ogrenci3=["Çınar","Turan",2017,[60,60,90]]
+
+# 9- öğrencilerin yaşlarını hesaplayınız
+yaşYiğit=2024-ogrenci1[2]
+yaşAda=2024-ogrenci2[2]
+yaşÇınar=2024-ogrenci3[2]
+
+# 11- Öğrencilerin not ortalamalarını hesaplayınız.
+ortalamaYiğit=(ogrenci1[3][0]+ogrenci1[3][1]+ogrenci1[3][2])/3
+ortalamaAda=(ogrenci2[3][0]+ogrenci2[3][1]+ogrenci2[3][2])/3
+ortalamaÇınar=(ogrenci3[3][0]+ogrenci3[3][1]+ogrenci3[3][2])/3
+
+print(ortalamaYiğit)
+print(ortalamaAda)
+print(ortalamaÇınar)
